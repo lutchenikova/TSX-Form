40a6022d239265ca1cee7f4f3602466d57f8b811
Lutchenikova <lutchenikova@gmail.com>
2023-11-17 00:57:00 -0300

    [develop] Add prop isDisabled on Button component and chage its style

--- TSX-Form/src/components/Button.tsx
+++ TSX-Form/src/components/Button.tsx
@@ -3,12 +3,13 @@ import { FormId } from "./Form";
 type ButtonProps = {
 	form: FormId;
 	isDisabled?: boolean;
+	onSubmit?: () => void;
 };
 
-export const Button = ({ form, isDisabled }: ButtonProps) => {
+export const Button = ({ form, isDisabled, onSubmit }: ButtonProps) => {
 	return (
 		<div className="gridButton">
-			<button disabled={isDisabled} form={form} type="button">
+			<button disabled={isDisabled} form={form} type="button" onSubmit={onSubmit}>
 				Submit
 			</button>
 		</div>
